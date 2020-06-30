5f37c0d79739f6a1af01652f72d786ad4470c7f6
Ivan Nefagin <dfyz0r@list.ru>
2020-07-01 00:56:33 +0300

    загрузка файлов

---
+++ js/main.js
@@ -0,0 +1,75 @@
+let flagDot = false,
+    leftBracketCount=0,
+    rightBracketCount=0;
+
+function addX(x) {
+    const answerStr = document.getElementById('input4c'),
+        lastElem = answerStr.value.slice(-1);
+
+    if (lastElem !== ')') {
+        if ((lastElem === "." && x === ".") || (lastElem === '/' && x === 0)){
+        } else {
+            if (x === ".") {
+                if (flagDot === false) {
+                    answerStr.value += x;
+                    flagDot = true;
+                }
+            } else {
+                answerStr.value += x;
+            }
+
+        }
+    }
+}
+
+function addOperation(x) {
+    const answerStr = document.getElementById('input4c'),
+        lastElem = answerStr.value.slice(-1);
+
+    if ((lastElem === '+' || lastElem === '-'
+        || lastElem === '*' || lastElem === '/' || lastElem === '.' || lastElem === '')) {
+    } else {
+        answerStr.value += x;
+        flagDot = false;
+    }
+}
+
+function addBracket(side='l') {
+    const answerStr = document.getElementById('input4c'),
+        lastElem = answerStr.value.slice(-1);
+
+    if (side === 'r' && leftBracketCount > rightBracketCount  && (lastElem !== '(' || lastElem !== '.')) {
+        rightBracketCount += 1;
+        answerStr.value += ')'
+    }
+    if (side === 'l' && (lastElem === '' || lastElem === '+' || lastElem === '-'
+        || lastElem === '*' || lastElem === '/') ) {
+        leftBracketCount += 1;
+        answerStr.value += '(';
+    }
+}
+
+function answer() {
+    const answerStr = document.getElementById('input4c');
+
+    if (answerStr.value) {
+        answerStr.value = eval(answerStr.value);
+    }
+}
+
+function clearAll() {
+    const answerStr = document.getElementById('input4c');
+
+    answerStr.value = '';
+    flagDot = false;
+}
+
+function clearEntry() {
+    const answerStr = document.getElementById('input4c'),
+        lastElem = answerStr.value.slice(-1);
+
+    if (lastElem === '.') {
+        flagDot = false;
+    }
+    answerStr.value = answerStr.value.slice(0,-1);
+}
